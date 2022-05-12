95adec4f0e7233973aa7ea4038c3e5e4b79dfa3a
mojan <hojat72elect@gmail.com>
2022-05-11 22:08:43 -0400

    nothing

--- core/src/main/java/ca/sudbury/hojat/core/domain/Photo.kt
+++ core/src/main/java/ca/sudbury/hojat/core/domain/Photo.kt
@@ -3,7 +3,11 @@ package ca.sudbury.hojat.core.domain
 /**
  * Created by Hojat Ghasemi at 2022-04-24
  * Contact the author at "https://github.com/hojat72elect"
+ *
+ * The main data object that will be used throughout the project.
+ *
  */
+
 data class Photo(
     val id: String?,
     val likes: Int,

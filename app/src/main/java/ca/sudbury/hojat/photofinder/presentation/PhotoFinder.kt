7599b173ecb32badcd60c84e1706762d4708de08
mojan <hojat72elect@gmail.com>
2022-05-16 17:52:27 -0400

    DataSource and its factory.

---
+++ app/src/main/java/ca/sudbury/hojat/photofinder/presentation/PhotoFinder.kt
@@ -0,0 +1,12 @@
+package ca.sudbury.hojat.photofinder.presentation
+
+/**
+ * Created by Hojat Ghasemi at 2022-05-16
+ * Contact the author at "https://github.com/hojat72elect"
+ */
+object PhotoFinder {
+    internal const val pageSize = 20
+    internal const val accessKey = "_htwco9giNp8TaA8kshIB3VP0eUZq5Y4pFWHLQuNPRU"
+    internal const val secretKey = "2LCXsb9sP2coqIQ0McGNzgWJZsPVLL6Lcvr-HGO5Ydc"
+    internal const val isLoggingEnabled = false
+}

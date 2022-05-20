3b4d35cdbd0448ee85a05a90c78571270366f605
mojan <hojat72elect@gmail.com>
2022-05-20 11:56:21 -0400

    There's a runtime error in the DI of this app. Looks like the application is not initialized before being used.

--- app/src/main/java/ca/sudbury/hojat/photofinder/presentation/MainActivity.kt
+++ app/src/main/java/ca/sudbury/hojat/photofinder/presentation/MainActivity.kt
@@ -1,13 +1,17 @@
 package ca.sudbury.hojat.photofinder.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import ca.sudbury.hojat.photofinder.Injector
 import ca.sudbury.hojat.photofinder.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var myViewModel: PhotoViewModel
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var myViewModel: PhotoViewModel
     private val TAG = "Main_Activity"
@@ -19,8 +23,17 @@ class MainActivity : AppCompatActivity() {
         binding.button.text = "click me as hard as you can!"
         setContentView(binding.root)
 
+        myViewModel = ViewModelProvider(
+            this,
+            Injector.createPickerViewModelFactory()
+        )[PhotoViewModel::class.java]
 
+        myViewModel.photosLiveData.observe(this, Observer {
 
+            Log.d("Resulting_Photo", it[2]?.links?.html.toString())
+        })
+
+        myViewModel.registerPhotosLiveData()
     }
 
 

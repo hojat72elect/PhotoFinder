71eafaa079828ec2bf4a3f2c5a597a970098cce8
mojan <hojat72elect@gmail.com>
2022-05-24 00:58:47 -0400

    MainActivity.kt was completed as well

--- app/src/main/java/ca/sudbury/hojat/photofinder/presentation/MainActivity.kt
+++ app/src/main/java/ca/sudbury/hojat/photofinder/presentation/MainActivity.kt
@@ -1,25 +1,19 @@
 package ca.sudbury.hojat.photofinder.presentation
 
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import ca.sudbury.hojat.photofinder.R
 import ca.sudbury.hojat.photofinder.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+/**
+ * This activity acts as the host for all fragments in the application
+ * @author Hojat Ghasemi
+ */
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
-    private val TAG = "Main_Activity_TAG"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-        binding = DataBindingUtil.setContentView(
-                this, R.layout.activity_main
-        ) // for easy connection of Activity with xml files.
-
+        setContentView(ActivityMainBinding.inflate(layoutInflater).root)
     }
 }
